eb29d55046a71df54f2b32962439619c85c97fca
Mitch Gohman <mitchell.gohman@travelctm.com>
2022-02-14 19:38:24 -0700

    Updatnig services

--- src/React/Services/ServiceGallery/ServiceGallery.jsx
+++ src/React/Services/ServiceGallery/ServiceGallery.jsx
@@ -3,14 +3,18 @@ import styled from 'styled-components';
 
 import ServiceGalleryItem from './ServiceGalleryItem.jsx';
 
-const ServiceGallery = ({services}) => {
+const ServiceGallery = ({services, chosenCategory}) => {
 
     return (
         <ServiceGalleryStyled className='ServiceGallery'>
             {
-                services.map((service, idx) => {
-                    return <ServiceGalleryItem key={ idx } service={ service }/>
-                })
+                services
+                    .filter((service) => {
+                        return (chosenCategory === 'All' || service.category === chosenCategory);
+                    })
+                    .map((service, idx) => {
+                        return <ServiceGalleryItem key={ idx } service={ service }/>
+                    })
             }
         </ServiceGalleryStyled>
     );

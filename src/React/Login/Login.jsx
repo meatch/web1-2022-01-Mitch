83ea466de618a84e6f583aa182628573e3e1deb9
Mitch Gohman <mitchell.gohman@travelctm.com>
2022-03-03 17:49:45 -0700

    Adding Login Form

--- src/React/Login/Login.jsx
+++ src/React/Login/Login.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/* Components ---------------------------*/
+import UniversalForm, { Input, Textarea, SubmitButton } from 'React/common/UniversalForm/UniversalForm.jsx';
+
 const Login = () => {
+
+    const onSubmitHandler = ({fields, payload}) => {
+        console.log('onSubmit fields, payload', fields, payload);
+    }
 
     return (
         <LoginStyled className='Login'>
             <h1>Login</h1> 
+            <UniversalForm
+                apiUrl='/user/login'
+                onSubmit={ onSubmitHandler }
+            >
+                <Input 
+                    label='Username'
+                    id='username'
+                    type='text'
+                    placeholder='username@email.com'
+                    rules={ ['required', 'email'] }
+                />
+                <Input 
+                    label='Password'
+                    id='password'
+                    type='password'
+                    rules={ ['required'] }
+                />
+                <SubmitButton>Send Email</SubmitButton>
+            </UniversalForm>
         </LoginStyled>
     );
 }
